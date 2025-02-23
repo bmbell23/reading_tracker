1fa5a6669edc0c5a05f2dad06f6f3287752267b5
brandon bell <bbell@ddn.com>
2025-02-23 12:30:20 -0700

    Adding the database to source control.

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ def create_venv():
 if __name__ == "__main__":
     setup(
         name="reading_list",
-        version="1.6.0",
+        version="1.6.1",
         packages=find_packages(include=['src', 'src.*', 'tests', 'tests.*', 'scripts', 'scripts.*']),
         package_dir={'': '.'},
         package_data={
